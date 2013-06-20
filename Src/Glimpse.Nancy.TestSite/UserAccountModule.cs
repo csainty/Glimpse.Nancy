39b51137285d4c1f3da48e8cc907db22b244a6fd
Chris Sainty <csainty@hotmail.com>
2013-06-20 17:23:34 +0200

    Added a "Modules" tab which shows all the Nancy modules and their routes

---
+++ Src/Glimpse.Nancy.TestSite/UserAccountModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace Glimpse.Nancy.TestSite
+{
+    public class UserAccountModule : NancyModule
+    {
+        public UserAccountModule()
+            : base("/users")
+        {
+            Get["/signin"] = _ => 404;
+            Post["/signin"] = _ => 404;
+            Put["/{userName}/{password}/wtf"] = _ => 404;
+            Get["/admin", ctx => false] = _ => 404;
+        }
+    }
+}
---
+++ Src/Glimpse.Nancy/Tabs/Modules.cs
@@ -0,0 +1,32 @@
+﻿using Glimpse.Core.Extensibility;
+using Glimpse.Nancy.Models;
+using Nancy;
+
+namespace Glimpse.Nancy.Tabs
+{
+    public class Modules : NancyTab, IKey
+    {
+        private readonly INancyModuleCatalog catalog;
+
+        public Modules(INancyModuleCatalog catalog)
+        {
+            this.catalog = catalog;
+        }
+
+        public override object GetData(ITabContext context)
+        {
+            var ctx = context.GetNancyContext();
+            return new ModulesModel(ctx, catalog);
+        }
+
+        public override string Name
+        {
+            get { return "Modules"; }
+        }
+
+        public string Key
+        {
+            get { return "glimpse_nancy_modules"; }
+        }
+    }
+}
