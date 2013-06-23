c5e36b9aa577a98182eedda40ea57e82a0a80480
Chris Sainty <csainty@hotmail.com>
2013-06-23 21:25:03 +0200

    Added tab for registered view models

---
+++ Src/Glimpse.Nancy/Models/ViewEnginesModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nancy.ViewEngines;
+
+namespace Glimpse.Nancy.Models
+{
+    public class ViewEnginesModel : List<ViewEngineModel>
+    {
+        public ViewEnginesModel(NancyContext ctx, IEnumerable<IViewEngine> viewEngines)
+        {
+            this.AddRange(viewEngines.Select(x => new ViewEngineModel(x)));
+        }
+    }
+
+    public class ViewEngineModel
+    {
+        private readonly IViewEngine viewEngine;
+
+        public ViewEngineModel(IViewEngine viewEngine)
+        {
+            this.viewEngine = viewEngine;
+        }
+
+        public string Name { get { return this.viewEngine.GetType().Name; } }
+
+        public IEnumerable<string> Extensions { get { return this.viewEngine.Extensions; } }
+    }
+}
---
+++ Src/Glimpse.Nancy/Tabs/ViewEngines.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Glimpse.Core.Extensibility;
+using Glimpse.Nancy.Models;
+using Nancy.ViewEngines;
+
+namespace Glimpse.Nancy.Tabs
+{
+    public class ViewEngines : NancyTab, IKey
+    {
+        private readonly IEnumerable<IViewEngine> viewEngines;
+
+        public ViewEngines(IEnumerable<IViewEngine> viewEngines)
+        {
+            this.viewEngines = viewEngines;
+        }
+
+        public override object GetData(ITabContext context)
+        {
+            var ctx = context.GetNancyContext();
+            return new ViewEnginesModel(ctx, this.viewEngines);
+        }
+
+        public override string Name
+        {
+            get { return "View Engines"; }
+        }
+
+        public string Key
+        {
+            get { return "nancy_glimpse_viewengines"; }
+        }
+    }
+}
