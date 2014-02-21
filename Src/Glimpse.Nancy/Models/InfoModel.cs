f460624fd81559e57544fe9901ae7d590d315509
Chris Sainty <csainty@hotmail.com>
2014-02-21 07:37:13 +0100

    Added all settings to Info tab

--- Src/Glimpse.Nancy/Models/InfoModel.cs
+++ Src/Glimpse.Nancy/Models/InfoModel.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using Nancy;
 using Nancy.Bootstrapper;
 
@@ -11,6 +12,8 @@ namespace Glimpse.Nancy.Models
     public class InfoModel
     {
         private readonly IRootPathProvider rootPathProvider;
+
+        private static readonly IEnumerable<Type> SettingTypes = new[] { typeof(StaticConfiguration) }.Union(typeof(StaticConfiguration).GetNestedTypes(BindingFlags.Static | BindingFlags.Public));
 
         public InfoModel(IRootPathProvider rootPathProvider, NancyInternalConfiguration configuration)
         {
@@ -25,15 +28,24 @@ namespace Glimpse.Nancy.Models
                     new[] { value.ToString() } :
                     ((IEnumerable<object>)value).Select(x => x.ToString());
             }
+
+            var properties = SettingTypes
+                .SelectMany(t => t.GetProperties(BindingFlags.Static | BindingFlags.Public))
+                .Where(x => x.PropertyType == typeof(bool));
+
+            Settings = from property in properties
+                       orderby property.Name
+                       let value = (bool)property.GetValue(null, null)
+                       select new SettingsModel
+                       {
+                           Name = Regex.Replace(property.Name, "[A-Z]", " $0"),
+                           Value = value
+                       };
         }
 
         public string NancyVersion { get { return string.Format("v{0}", typeof(NancyContext).Assembly.GetName().Version.ToString()); } }
 
         public string RootPath { get { return this.rootPathProvider.GetRootPath(); } }
-
-        public bool TracesEnabled { get { return !StaticConfiguration.DisableErrorTraces; } }
-
-        public bool CaseSensitivity { get { return StaticConfiguration.CaseSensitive; } }
 
         public string LocatedBootstrapper { get { return NancyBootstrapperLocator.Bootstrapper.GetType().ToString(); } }
 
@@ -68,5 +80,14 @@ namespace Glimpse.Nancy.Models
         }
 
         public Dictionary<string, IEnumerable<string>> Configuration { get; set; }
+
+        public IEnumerable<SettingsModel> Settings { get; set; }
+
+        public class SettingsModel
+        {
+            public string Name { get; set; }
+
+            public bool Value { get; set; }
+        }
     }
 }
