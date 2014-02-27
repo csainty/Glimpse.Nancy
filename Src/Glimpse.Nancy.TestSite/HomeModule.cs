e6cce56274ea3156ac01c15e581445850725d6b0
Chris Sainty <csainty@hotmail.com>
2014-02-27 22:23:25 +0100

    Added Glimpse.ADO to test site to ensure custom tabs still work

--- Src/Glimpse.Nancy.TestSite/HomeModule.cs
+++ Src/Glimpse.Nancy.TestSite/HomeModule.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data.SQLite;
 using Nancy;
+using Dapper;
+using System.Data.Common;
 
 namespace Glimpse.Nancy.TestSite
 {
@@ -10,6 +13,16 @@ namespace Glimpse.Nancy.TestSite
             Get["/"] = _ =>
             {
                 Context.Trace.TraceLog.WriteLog(sb => sb.AppendLine("User requested home page"));
+
+                var factory = DbProviderFactories.GetFactory("System.Data.SQLite");
+
+                using (var conn = factory.CreateConnection())
+                {
+                    conn.ConnectionString = "Data Source=:memory:";
+                    conn.Open();
+                    conn.Execute("CREATE TABLE Foo (Id INTEGER PRIMARY KEY)");
+                    conn.Query("SELECT * FROM Foo");
+                }
 
                 return Negotiate
                     .WithModel(new { Foo = "Bar", Now = DateTime.Now })
