6735e78ff0d2c63fe5e3aed22ebe28988035a4a3
Chris Sainty <csainty@hotmail.com>
2014-06-07 16:45:06 +0200

    Added RouteResolver timeline inspector

--- Src/Glimpse.Nancy/TimelineInspectors.cs
+++ Src/Glimpse.Nancy/TimelineInspectors.cs
@@ -13,6 +13,7 @@ namespace Glimpse.Nancy
             config.ViewFactory = typeof(GlimpseViewFactory<>).MakeGenericType(config.ViewFactory);
             config.ViewResolver = typeof(GlimpseViewResolver<>).MakeGenericType(config.ViewResolver);
             config.ViewLocator = typeof(GlimpseViewLocator<>).MakeGenericType(config.ViewLocator);
+            config.RouteResolver = typeof(GlimpseRouteResolver<>).MakeGenericType(config.RouteResolver);
         }
 
         public static IEnumerable<Type> InspectViewEngines(IEnumerable<Type> viewEngines)
---
+++ Src/Glimpse.Nancy/Wrappers/GlimpseRouteResolver.cs
@@ -0,0 +1,27 @@
+﻿using Nancy;
+using Nancy.Routing;
+
+namespace Glimpse.Nancy.Wrappers
+{
+    internal class GlimpseRouteResolver<T> : IRouteResolver where T : IRouteResolver
+    {
+        private readonly IRouteResolver routeResolver;
+
+        public GlimpseRouteResolver(T routeResolver)
+        {
+            this.routeResolver = routeResolver;
+        }
+
+        public ResolveResult Resolve(NancyContext context)
+        {
+            var request = context.TryGetRequestContext();
+
+            if (request == null)
+            {
+                return routeResolver.Resolve(context);
+            }
+
+            return request.TimeAction("RouteResolver", "Resolve: " + context.Request.Path, () => routeResolver.Resolve(context));
+        }
+    }
+}
